cd81f1a8c79601eb85d1041b1c615bcb43aec7e8
Davina Leong <leong.shi.yun@gmail.com>
2023-09-22 20:59:07 +0800

    WIP retrieve contentful model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const _ = require("lodash")
 const { PrismaClient } = require("@prisma/client")
 const { richTextFromMarkdown } = require("@contentful/rich-text-from-markdown")
+const { environment, contentTypeIds } = require("./lib/contentful")
 const { logFunction, logValue } = require("./lib/helpers")
 
 const prisma = new PrismaClient()
@@ -8,9 +9,38 @@ const prisma = new PrismaClient()
 async function main() {
   logFunction(`main`)
 
-  const { text } = await prisma.posts.findFirst({ where: { id: 1 } })
-  const richText = await richTextFromMarkdown(text)
-  logValue(`richText`, richText)
+  const {
+    title,
+    slug,
+    subtitle,
+    summary,
+    text,
+    featured,
+    status,
+    published_at,
+  } = await prisma.posts.findFirst({ where: { id: 1 } })
+  //   const richText = await richTextFromMarkdown(text)
+  //   logValue(`richText`, richText)
+  await client.entry.create({
+    contentTypeId: contentTypeIds.posts,
+    fields: {
+      title: {
+        "en-US": title,
+      },
+      slug: {
+        "en-US": slug,
+      },
+      excerpt: {
+        "en-US": summary,
+      },
+      subtitle: {
+        "en-US": subtitle,
+      },
+      // body: await richTextFromMarkdown(text),
+      publishedAt: published_at,
+      featured: featured === true,
+    },
+  })
 }
 
 main()
