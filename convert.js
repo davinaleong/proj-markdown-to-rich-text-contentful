3d633ef4aecb9cc12278caa6a9087cda4b9c314e
Davina Leong <davina.leong.sy@gmail.com>
2023-12-26 19:23:04 +0800

    WIP

--- convert.js
+++ convert.js
@@ -1,10 +1,14 @@
 const _ = require("lodash")
 const { PrismaClient } = require("@prisma/client")
 const { richTextFromMarkdown } = require("@contentful/rich-text-from-markdown")
-const { logFunction, logValue } = require("./lib/helpers")
+const { logFunction, logValue } = require("./lib/helpers.lib")
 
 const prisma = new PrismaClient()
 
-const { text } = await prisma.posts.findFirst({ where: { id: 1 } })
-const richText = await richTextFromMarkdown(text)
-logValue(`richText`, richText)
+async function main() {
+  const { text } = await prisma.posts.findFirst({ where: { id: 1 } })
+  const richText = await richTextFromMarkdown(text)
+  logValue(`richText`, richText)
+}
+
+main()
--- index.js
+++ index.js
@@ -1,55 +1,68 @@
 const { PrismaClient } = require("@prisma/client")
-const { envVars } = require("contentful-management")
+const contentful = require("contentful-management")
 const { richTextFromMarkdown } = require("@contentful/rich-text-from-markdown")
+const { BLOCKS } = require("@contentful/rich-text-types")
 const { logFunction, logValue } = require("./lib/helpers.lib")
-const { logFunction, logValue } = require("./lib/helpers.lib")
-const { contentTypeIds } = require("./lib/contentful.lib")
-
-const prisma = new PrismaClient()
-
-const { title, slug, subtitle, summary, text, featured, status, published_at } =
-  prisma.posts.findFirst({ where: { id: 1 } })
-
-const richText = richTextFromMarkdown(text)
-logValue(`richText`, richText)
+const { envVars, contentTypeIds } = require("./lib/contentful.lib")
 
 const client = contentful.createClient({
   accessToken: envVars.accessToken,
 })
 
-const entry = {
-  fields: {
-    title: {
-      "en-US": title,
-    },
-    slug: {
-      "en-US": slug,
-    },
-    excerpt: {
-      "en-US": summary,
-    },
-    subtitle: {
-      "en-US": subtitle,
-    },
-    body: {
-      "en-US": richText,
-    },
-    featured: {
-      "en-US": featured === true,
-    },
-  },
-}
+const prisma = new PrismaClient()
 
-client
-  .getSpace(envVars.spaceId)
-  .then((space) => space.getEnvironment(envVars.environmentId))
-  .then((environment) => environment.createEntryWithId(contentTypeIds.posts), {
-    fields: {
-      title: {
-        "en-us": title,
+prisma.posts
+  .findFirst({ where: { id: 1 } })
+  .then((post) => {
+    const {
+      title,
+      slug,
+      subtitle,
+      summary,
+      text,
+      featured,
+      status,
+      published_at,
+    } = post
+    // console.log("post", post)
+
+    const richTextOptions = {
+      renderNode: {
+        [BLOCKS.PARAGRAPH]: (node, next) => {
+          return {
+            nodeType: "paragraph",
+            content: next(node.content),
+          }
+        },
       },
-      slug: {
-        "en-us": slug,
-      },
-    },
+    }
+
+    const richText = richTextFromMarkdown(text, richTextOptions)
+
+    // This API call will request a space with the specified ID
+    client.getSpace(envVars.spaceId).then((space) => {
+      // This API call will request an environment with the specified ID
+      space.getEnvironment(envVars.environmentId).then((environment) => {
+        // let's get a content type
+        environment.getContentType(contentTypeIds.posts).then((contentType) => {
+          // console.log("post > environment", title)
+          // console.log("contentType", contentType)
+
+          // and now let's update its name
+          contentType.fields.title = title
+          contentType.fields.slug = slug
+          contentType.fields.excerpt = summary
+          contentType.fields.subtitle = subtitle
+          contentType.fields.body = richText.content
+
+          contentType.create()
+
+          // contentType.update().then((updatedContentType) => {
+          //   console.log("updatedContentType", updatedContentType)
+          //   console.log("Update was successful")
+          // })
+        })
+      })
+    })
   })
+  .catch((error) => console.error(error))
