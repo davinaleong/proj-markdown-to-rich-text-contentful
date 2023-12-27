4f4097152307d9c73c74c6106cccce04193d5daf
Davina Leong <davina.leong.sy@gmail.com>
2023-12-27 21:38:19 +0800

    WIP open ai

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const prompt = "Tell me a joke."
-const maxTokens = 60
+const dotenv = require("dotenv").config()
+const OpenAI = require("openai")
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+
+async function main(prompt) {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+  })
+
+  console.log(completion.choices[0])
+}
+
+main()
